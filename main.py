4bb20445ba808a9ea6b8d4ad43a2d2981612acc4
graffsmile <sevulybin@yandex.ru>
2024-12-28 16:22:03 +0500

    update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import os
+from idlelib.editor import keynames
+from operator import itemgetter
 from os.path import split
 from pprint import pprint
 import csv
 import re
+import itertools
 
 # читаем адресную книгу в формате CSV в список contacts_list
 
@@ -49,19 +52,55 @@ def phones_fixed(file_name, n_file):
         fix = fw.write(fixed_phones)
     return fix
 
+# def names_merges(contacts_list):
+
 
   # TODO 2: сохраните получившиеся данные в другой файл
   # код для записи файла в формате CSV
 def write(new_file, dict):
-    # contacts = splited_name
-    keys = list(dict[0].keys())
+    # list_dict = dict
+    contacts_dict = {}
+    keys = dict[0].keys()
+
+    for contacts in dict:
+        lastname = dict[1]
+        firstname = dict[2]
+        # surname = contacts[2]
+        # organization = contacts[3]
+        # position = contacts[4]
+        # phone = phones_fixed(contacts[5])
+        # email = contacts[6]
+        # key = (lastname, firstname)
+        result_list = list()
+        for new_contact in dict[1:]:
+            new_lastname = new_contact['lastname']
+            new_firstname = new_contact['firstname']
+            if lastname == new_lastname and firstname == new_firstname:
+                if contacts[2] == '':
+                    contacts[2] = new_contact[2]
+                if contacts[3] == '':
+                    contacts[3] = new_contact[3]
+                if contacts[4] == '':
+                    contacts[4] = new_contact[4]
+                if contacts[5] == '':
+                    contacts[5] = new_contact[5]
+                if contacts[6] == '':
+                    contacts[6] = new_contact[6]
+                # result_list = list()
+                if contacts not in dict:
+                    result_list.append(contacts)
+                # return result_list
+        pprint(result_list)
+
     with open(new_file, "w", encoding="utf-8") as f:
         datawriter = csv.writer(f, delimiter=',')
     # Вместо contacts_list подставьте свой список
-        datawriter.writerows(keys)
+        datawriter.writerow(keys)
         for items in dict:
-            datawriter.writerows(items)
-        # return datawriter.writerows(contacts)
+            datawriter.writerow(items.values())
+        return datawriter
+
+
 
 if __name__ == '__main__':
     file_name = 'phonebook_raw.csv'
@@ -70,5 +109,5 @@ if __name__ == '__main__':
     phones_fixed(file_name, n_file='nfile.csv')
     splited_name = split_name(file_name='nfile.csv')
     os.remove('nfile.csv')
-    pprint(splited_name)
+    # pprint(splited_name)
     write(new_file, splited_name)
